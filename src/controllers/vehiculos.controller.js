7d9949092fadb57db30a200dec20d8be4f124c7a
Hans <kennerbarahona.14@hotmail.com>
2024-06-10 18:48:51 -0600

    fixes

--- src/controllers/vehiculos.controller.js
+++ src/controllers/vehiculos.controller.js
@@ -45,12 +45,6 @@ export const createVehiculo = async (req, res) => {
     try {
         const { id, marca, modelo, anio, kilometraje, foto, idConductorAsignado } = req.body;
 
-        // Verificar si el conductor existe
-        const [conductor] = await pool.query("SELECT * FROM Conductores WHERE id = ?", [idConductorAsignado]);
-        if (conductor.length === 0) {
-            return res.status(404).json({ message: "Conductor no encontrado" });
-        }
-
         const [rows] = await pool.query(
             "INSERT INTO Vehiculos (id, marca, modelo, anio, kilometraje, foto, idConductorAsignado) VALUES (?, ?, ?, ?, ?, ?, ?)",
             [id, marca, modelo, anio, kilometraje, foto, idConductorAsignado]
@@ -65,12 +59,6 @@ export const updateVehiculo = async (req, res) => {
     try {
         const { id } = req.params;
         const { marca, modelo, anio, kilometraje, foto, idConductorAsignado } = req.body;
-
-        // Verificar si el conductor existe
-        const [conductor] = await pool.query("SELECT * FROM Conductores WHERE id = ?", [idConductorAsignado]);
-        if (conductor.length === 0) {
-            return res.status(404).json({ message: "Conductor no encontrado" });
-        }
 
         const [result] = await pool.query(
             "UPDATE Vehiculos SET marca = IFNULL(?, marca), modelo = IFNULL(?, modelo), anio = IFNULL(?, anio), kilometraje = IFNULL(?, kilometraje), foto = IFNULL(?, foto), idConductorAsignado = IFNULL(?, idConductorAsignado) WHERE id = ?",
