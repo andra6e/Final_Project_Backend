aa43b5f27d4bf1f951fefb0e7c445d456e7bb5b1
Hans <kennerbarahona.14@hotmail.com>
2024-06-13 23:20:23 -0600

    change port

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "12345";
 export const DB_DATABASE = process.env.DB_DATABASE || "app";
-export const DB_PORT = process.env.DB_PORT || 5432;
+export const DB_PORT = process.env.DB_PORT || 3306;
